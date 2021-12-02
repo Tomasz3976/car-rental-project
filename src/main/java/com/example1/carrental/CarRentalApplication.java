a7d8ea9f165a3521cf69585d6a5fef0a1424e31c
tomeczek2886 <tomeczek2886@gmail.com>
2021-12-02 15:52:35 +0100

    feat: add user and carpanel controller

--- src/main/java/com/example1/carrental/CarRentalApplication.java
+++ src/main/java/com/example1/carrental/CarRentalApplication.java
@@ -1,7 +1,18 @@
 package com.example1.carrental;
 
+import com.example1.carrental.constant.FuelType;
+import com.example1.carrental.constant.GearBoxType;
+import com.example1.carrental.domain.Car;
+import com.example1.carrental.domain.CarPackage;
+import com.example1.carrental.domain.CarParameters;
+import com.example1.carrental.domain.Role;
+import com.example1.carrental.dto.UserSaveDto;
+import com.example1.carrental.service.CarService;
+import com.example1.carrental.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -12,4 +23,7 @@ public class CarRentalApplication {
 		SpringApplication.run(CarRentalApplication.class, args);
 	}
 
-}
+
+
+	}
+
