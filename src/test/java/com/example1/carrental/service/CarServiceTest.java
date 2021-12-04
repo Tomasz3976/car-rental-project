5b3ae328d455cf99c60a3f1db52547f43d185cd7
tomeczek2886 <tomeczek2886@gmail.com>
2021-12-04 16:28:38 +0100

    fix: deleting not essential dependency

--- src/test/java/com/example1/carrental/service/CarServiceTest.java
+++ src/test/java/com/example1/carrental/service/CarServiceTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
--- src/test/java/com/example1/carrental/service/UserServiceTest.java
+++ src/test/java/com/example1/carrental/service/UserServiceTest.java
@@ -9,14 +9,10 @@ import com.example1.carrental.repo.RoleRepo;
 import com.example1.carrental.repo.UserRepo;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.Optional;
