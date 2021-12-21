c488a29b210d12ad179e1c7107ddc2ce1fa66ecf
tomeczek2886 <tomeczek2886@gmail.com>
2021-12-21 19:26:27 +0100

    fix: bug in logged-in-user

--- src/main/java/com/example1/carrental/security/LoggedInUser.java
+++ src/main/java/com/example1/carrental/security/LoggedInUser.java
@@ -2,6 +2,7 @@ package com.example1.carrental.security;
 
 import com.example1.carrental.domain.User;
 import com.example1.carrental.repo.UserRepo;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -17,8 +18,8 @@ public class LoggedInUser {
 
         public User getUser() {
 
-                Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-                String username = ((UserDetails) principal).getUsername();
+                Authentication principal = SecurityContextHolder.getContext().getAuthentication();
+                String username = principal.getName();
                 return userRepo.findByUsername(username).orElseThrow();
 
         }
