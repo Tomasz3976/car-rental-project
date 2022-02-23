f9bfd24a191124f9b3936781589dda15fa924235
tomeczek2886 <tomeczek2886@gmail.com>
2022-02-23 14:32:46 +0100

    style: delete redundant imports

--- src/test/java/com/example1/carrental/service/RegistrationServiceTest.java
+++ src/test/java/com/example1/carrental/service/RegistrationServiceTest.java
@@ -18,7 +18,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
