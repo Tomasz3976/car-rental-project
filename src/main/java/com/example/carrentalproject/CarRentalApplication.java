b2e22aeed15206d38f255fa7c50801ced8c084aa
tomeczek2886 <tomeczek2886@gmail.com>
2022-02-27 20:33:51 +0100

    feat: swagger config

--- src/main/java/com/example/carrentalproject/CarRentalApplication.java
+++ src/main/java/com/example/carrentalproject/CarRentalApplication.java
@@ -3,11 +3,9 @@ package com.example.carrentalproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
-@EnableSwagger2
 public class CarRentalApplication {
 
 	public static void main(String[] args) {
